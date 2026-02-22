c6db89a3e0a10eca5936d48537f2e935dae3deb9
Cheez444 <ash.lee.esler@gmail.com>
2026-02-22 13:13:22 -0600

    leds
    coded candle and candle command (subject to change)

---
+++ 2026Rebuilt/src/main/java/frc/robot/commands/CandleCommand.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.TurretHood;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class CandleCommand extends Command {
+  LEDs leds;
+  Turret turret;
+  TurretHood hood;
+  Shooter shooter;
+  /** Creates a new CandleCommand. */
+  public CandleCommand(LEDs l, Turret t, TurretHood h, Shooter s) {
+    leds = l;
+    turret = t;
+    hood = h;
+    shooter = s;
+    addRequirements(leds);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (turret.isAimed() && hood.isInPosiiton() && shooter.shooterAtSpeed()) {
+      leds.setOrangeStatic();
+    } else {
+      leds.setBlueStatic();
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ 2026Rebuilt/src/main/java/frc/robot/subsystems/LEDs.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.RGBWColor;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDs extends SubsystemBase {
+  CANdle candle;
+  /** Creates a new LEDs. */
+  public LEDs() {
+    candle = new CANdle(0, new CANBus("1912CANivore"));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void setBlueStatic() {
+    final SolidColor request = new SolidColor(0, 400);
+    candle.setControl(request.withColor(new RGBWColor(0, 140, 220)));
+  }
+
+  public void setOrangeStatic() {
+    final SolidColor request = new SolidColor(0, 400);
+    candle.setControl(request.withColor(new RGBWColor(230, 40, 0)));
+  }
+}
