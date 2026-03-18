419c136db01874d574242e23251c9b9ff448bd5f
Cheez444 <ash.lee.esler@gmail.com>
2026-03-17 20:43:20 -0500

    almost time
    magnolia tomorrow, auto added to robotcontainer

--- 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -204,8 +204,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return new Command() {
-      
-    };
+    return driveTrain.autoChooser.getSelected();
   }
 }
